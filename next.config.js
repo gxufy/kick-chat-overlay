96d4eee5ee31fe90b3e47a250b60b46650048797
Isaac <unstoppableboy99@gmail.com>
2025-10-04 11:20:21 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['files.kick.com', 'cdn.7tv.app'],
     unoptimized: true
   }
 }
-
-module.exports = nextConfig
